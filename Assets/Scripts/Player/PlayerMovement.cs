a9f522b55abf183f00c78744f9412a2ff7237cdf
André Costa <77451181+andrefnc4@users.noreply.github.com>
2022-10-07 14:46:45 +0100

    idle and walk animations fixed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,26 +10,77 @@ public class PlayerMovement : MonoBehaviour
     public float horizontalMove;
     public float verticalMove;
 
+    private Vector3 moveDirection;
+    private CharacterController controller;
+    private float moveSpeed;
+    private float walkSpeed;
+    private float runSpeed;
     private int speed = 150;
     private float jumpHeight = 3.9f;
 
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
-        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        Move();
+        //if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        //{
+        //    isMoving = true;
+        //    thePlayer.GetComponent<Animator>().SetBool("Idle", false);
+        //    thePlayer.GetComponent<Animator>().SetBool("Walk", true);
+        //    horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //    verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * jumpHeight;
+        //    thePlayer.transform.Rotate(0, horizontalMove, 0);
+        //    thePlayer.transform.Translate(0, 0, verticalMove);
+        //}
+        //else
+        //{
+        //    isMoving = false;
+        //    thePlayer.GetComponent<Animator>().SetBool("Walk", false);
+        //    thePlayer.GetComponent<Animator>().SetBool("Idle", true);
+        //}
+    }
+
+    private void Move()
+    {
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(0, 0, moveZ);
+        
+        if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
         {
-            isMoving = true;
-            thePlayer.GetComponent<Animator>().SetBool("Idle", false);
-            thePlayer.GetComponent<Animator>().SetBool("Walk", true);
-            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * jumpHeight;
-            thePlayer.transform.Rotate(0, horizontalMove, 0);
-            thePlayer.transform.Translate(0, 0, verticalMove);
+            // Walk
+            Walk();
         }
-        else
+        else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
         {
-            isMoving = false;
-            thePlayer.GetComponent<Animator>().SetBool("Walk", false);
-            thePlayer.GetComponent<Animator>().SetBool("Idle", true);
+            // Run
+            Run();
         }
+        else if (moveDirection == Vector3.zero)
+        {
+            // Idle
+            Idle();
+        }
+        moveDirection *= moveSpeed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+    }
+    private void Run()
+    {
+        moveSpeed = runSpeed;
     }
 }
