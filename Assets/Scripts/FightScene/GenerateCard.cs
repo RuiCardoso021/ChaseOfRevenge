dc3cc811b81f071301081549e52b1114107417a8
RuiCardoso021 <ruicardoso021@gmail.com>
2022-10-19 16:08:08 +0100

    fixed errors on cards exiption

--- Assets/Scripts/FightScene/GenerateCard.cs
+++ Assets/Scripts/FightScene/GenerateCard.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GenerateCard : MonoBehaviour
 {
    public GameObject Card;
+   private List<GameObject> cards = new List<GameObject>();
    private float numberCards = 4;
    private ReadJson dataCard;
    private Deck deck;
@@ -23,16 +25,15 @@ public class GenerateCard : MonoBehaviour
         for (int i=0; i<numberCards; i++)
         {
             //indice random
-
-            random = Random.Range(0, deck.cards.Length);
-            Debug.Log(random);
-      
-    
+            do
+            {
+                random = Random.Range(0, deck.cards.Length);
+            } while (cardsToGive.Contains(random));
             cardsToGive.Add(random);
             
             //generate card
             Card.GetComponent<ReadJson>().setDataCard(deck.cards[random].mana.ToString(), deck.cards[random].description, deck.cards[random].name, deck.cards[random].src);
-            Instantiate(Card, new Vector3(x,1,-7), Quaternion.identity);
+            cards.Add(Instantiate(Card, new Vector3(x,1,-7), Quaternion.identity));
             x+= 0.6f;
         }
    }
@@ -40,6 +41,13 @@ public class GenerateCard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            this.transform.localScale = new Vector3 (0.2f,0.2f,0.2f);
+        }
+        else
+        {
+            this.transform.localScale = new Vector3 (0.1f,0.1f,0.1f);
+        }
     }
 }
