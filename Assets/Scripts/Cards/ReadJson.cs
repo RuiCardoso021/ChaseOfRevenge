a14eb64ad0160c37e87c00c443ace6c13a55cbeb
RuiCardoso021 <ruicardoso021@gmail.com>
2022-10-19 01:36:28 +0100

    generate other cards

--- Assets/Scripts/Cards/ReadJson.cs
+++ Assets/Scripts/Cards/ReadJson.cs
@@ -11,6 +11,7 @@ public class ReadJson : MonoBehaviour
     
     [SerializeField] public TextMeshProUGUI _textMana;
     [SerializeField] public TextMeshProUGUI _textDescription;
+    [SerializeField] public TextMeshProUGUI _textName;
     [SerializeField] public Image _image;
  
 
@@ -19,33 +20,35 @@ public class ReadJson : MonoBehaviour
         deck = new Deck();
         deck = JsonUtility.FromJson<Deck>(jsonFile.text);
 
-        foreach (Card card in deck.cards)
-        {
-            Debug.Log("\n id: " + card.id
-            + "\n name: " + card.name 
-            + "\n number_effects: " + card.number_effects 
-            + "\n text: " + card.description 
-            + "\n mana: " + card.mana 
-            + "\n type: " + card.type
-            + "\n src: " + card.src);
 
-            _textMana.text = card.mana.ToString();
-            _textDescription.text = card.description;
-            _image.sprite = Resources.Load<Sprite>("imagesCards/" + card.src);
+        int random = Random.Range(0, deck.cards.Length);
+        _textMana.text = deck.cards[random].mana.ToString();
+        _textDescription.text = deck.cards[random].description;
+        _textName.text = deck.cards[random].name;
+        _image.sprite = Resources.Load<Sprite>("imagesCards/" + deck.cards[random].src);
+        
 
-            foreach (Ability ab in card.ability)
-            {
-                Debug.Log("\n type: " + ab.tag
-                + "\n value: " + ab.value
-                + "\n effect_quantity: " + ab.effect_quantity
-                + "\n type_effect: " + ab.type_effect);
-            }
-           //foreach (Ability item in abilityList)
-           //{
-           //    Debug.Log(item.type_effect);
-           //}
+        //for (int i = 0; i < deck.cards.Length; i++)
+        //{
+        //     Debug.Log("\n id: " + deck.cards[i].id
+        //    + "\n name: " + deck.cards[i].name 
+        //    + "\n number_effects: " + deck.cards[i].number_effects 
+        //    + "\n text: " + deck.cards[i].description 
+        //    + "\n mana: " + deck.cards[i].mana 
+        //    + "\n type: " + deck.cards[i].type
+        //    + "\n src: " + deck.cards[i].src);
+//
+        //    foreach (Ability ab in deck.cards[i].ability)
+        //    {
+        //        Debug.Log("\n type: " + ab.tag
+        //        + "\n value: " + ab.value
+        //        + "\n effect_quantity: " + ab.effect_quantity
+        //        + "\n type_effect: " + ab.type_effect);
+        //    }
+        //}
+      
 
-        }
+
         
         
     }
--- Assets/Scripts/FightScene/GenerateCard.cs
+++ Assets/Scripts/FightScene/GenerateCard.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class GenerateCard : MonoBehaviour
 {
-    // Instantiates prefabs in a circle formation
-   public GameObject prefab;
-   public int numberOfObjects = 20;
-   public float radius = 5f;
+   public GameObject block;
+   private int numberCards = 2;
+  
    void Start()
    {
-       for (int i = 0; i < numberOfObjects; i++)
-       {
-           float angle = i * Mathf.PI * 2 / numberOfObjects;
-           float x = Mathf.Cos(angle) * radius;
-           float z = Mathf.Sin(angle) * radius;
-           Vector3 pos = transform.position + new Vector3(x, 0, z);
-           float angleDegrees = -angle*Mathf.Rad2Deg;
-           Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
-           Instantiate(prefab, pos, rot);
-       }
+
+        for (int x=-2; x<numberCards; ++x)
+        {
+            Instantiate(block, new Vector3(x,1,-7), Quaternion.identity);
+        }
+             
    }
 
     // Update is called once per frame
