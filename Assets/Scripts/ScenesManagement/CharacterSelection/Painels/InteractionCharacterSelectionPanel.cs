2a38ab9f6c57f341e027787b13ba177129e22050
André Costa <77451181+andrefnc4@users.noreply.github.com>
2022-11-15 17:09:30 +0000

    inventory fixed

--- Assets/Scripts/ScenesManagement/CharacterSelection/Painels/InteractionCharacterSelectionPanel.cs
+++ Assets/Scripts/ScenesManagement/CharacterSelection/Painels/InteractionCharacterSelectionPanel.cs
@@ -8,17 +8,19 @@ public class InteractionCharacterSelectionPanel : MonoBehaviour
 {
 
     [SerializeField] private TextMeshProUGUI _textName;
-    //[SerializeField] private Image _imageProfile;
+    [SerializeField] private Image _imageProfile;
     [SerializeField] private TextMeshProUGUI _txtLife;
     [SerializeField] private TextMeshProUGUI _txtMana;
     [SerializeField] private TextMeshProUGUI _txtClassType;
     [SerializeField] private Image _healthBar;
 
+    private Character_cls player;
+
     public string Name;
     public float Health;
     public int Mana;
     public string ClassType;
-    //public Image ImageProfile;
+    public Sprite ImageProfile;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +29,13 @@ public class InteractionCharacterSelectionPanel : MonoBehaviour
         Name = "Nick Name";
         Mana = 1;
         ClassType = "class";
+        ImageProfile = player.GetComponent<Character_cls>().ImageProfile;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //_imageProfile = ImageProfile;
+        _imageProfile.sprite = ImageProfile;
         _textName.text = Name;
         _txtLife.text = Health.ToString();
         _txtMana.text = Mana.ToString();
