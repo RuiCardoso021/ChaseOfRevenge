bfb9400bc36f0669866cfbb56b8043ae05501f04
André Costa <77451181+andrefnc4@users.noreply.github.com>
2022-12-28 16:38:11 +0000

    npc behaviour

--- Assets/Scripts/NPC/NpcAINav.cs
+++ Assets/Scripts/NPC/NpcAINav.cs
@@ -5,26 +5,71 @@ using UnityEngine.AI;
 
 public class NpcAINav : MonoBehaviour
 {
-    public GameObject Destination;
-    NavMeshAgent npcAgent;
-    public static bool canInteract;
-    //public GameObject Player;
-    Character_Prefab player;
+    GameObject player;
+    Vector3 moveDirect;
+    CharacterController controller;
     Animator animator;
+    Vector3 firstPosition;
+    Vector3 velocity;
+    bool backToPosition = true;
+    bool colision = false;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float speed = 2;
+    [SerializeField] float radius = 5;
+
+    private void Start()
     {
-        npcAgent = GetComponent<NavMeshAgent>();
-        player = GameObject.Find("Character_Player").GetComponent<Character_Prefab>();
+        firstPosition = transform.position;
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        npcAgent.SetDestination(Destination.transform.position);
-            
-        animator.SetBool("Walk", true);
-        animator.SetBool("Idle", false);
+        if (player == null)
+            player = GameObject.Find(Global.findPlayer);
+
+        if (player != null)
+        {
+            CheckRadius();
+
+            if (controller != null)
+            {
+                if (!backToPosition && !colision)
+                {
+                    animator.SetInteger("Transition", 1);
+                    controller.Move(velocity * Time.deltaTime);
+                }
+                else animator.SetInteger("Transition", 0);
+
+            }
+            player = null;
+        }
+    }
+
+    private void CheckRadius()
+    {
+        if (Vector3.Distance(firstPosition, player.transform.position) < radius)
+        {
+            //if detect player on range, move to player
+            moveDirect = player.transform.position - transform.position;
+            backToPosition = false;
+
+            velocity = moveDirect * speed;
+            velocity.Normalize();
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(velocity), (speed * 2) * Time.deltaTime);
+        }
+        else
+        {
+            //move to first position
+            moveDirect = firstPosition - transform.position;
+            if (Vector3.Distance(firstPosition, transform.position) <= 0.6f)
+                backToPosition = true;
+            else backToPosition = false;
+
+            velocity = moveDirect * speed;
+            velocity.Normalize();
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(velocity), (speed * 2) * Time.deltaTime);
+        }
     }
 }
