6820ea4c887a7b3805453b4422ef8551154a63f4
André Costa <77451181+andrefnc4@users.noreply.github.com>
2022-12-29 14:39:36 +0000

    main menu design and credits

--- Assets/Scripts/ScenesManagement/Menu/MainMenu.cs
+++ Assets/Scripts/ScenesManagement/Menu/MainMenu.cs
@@ -4,8 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject creditsPanel;
     public void PlayGame() {
         SceneManager.LoadScene("CharacterSelection");
+    }
+
+    public void ShowCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
     }
 
     public void QuitGame() {
